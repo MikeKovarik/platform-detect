c9c0baa850fa2191bb71d83a50fb959d42db0773
Mike <kenr.mk@gmail.com>
2018-10-16 16:25:50 +0200

    slight rewrite + added tabletMode, orientation watch, touch/mouse watcher

--- index.mjs
+++ index.mjs
@@ -1,155 +1,187 @@
+var p = {}
+export default p
+
 // The app has a window to render DOM content in.
-var hasWindow = typeof navigator !== 'undefined' && typeof window !== 'undefined'
+p.window = typeof navigator !== 'undefined' && typeof window !== 'undefined'
 // NW.JS' background script loads into invisible empty HTML page.
 // Therefore NW.JS "main" script has all of window, document, navigator objects
 // and window.open() won't be null if App's window is launched from such "main".
-if (hasWindow && typeof nw !== 'undefined') {
+if (p.window && typeof nw !== 'undefined') {
 	try {
 		nw.Window.get()
 	} catch(err) {
-		hasWindow = false
+		p.window = false
 	}
 }
 // The app runs in terminal/console and can only use console.log.
-var isConsole = !hasWindow
+p.console = !p.window
 
-var ua = hasWindow ? navigator.userAgent : undefined
+var ua = p.window ? navigator.userAgent : undefined
+
+function registerQuery(query, handler) {
+	var mql = window.matchMedia(query)
+	handler(mql.matches)
+	var listener = () => handler(mql.matches)
+	mql.addListener(listener)
+	return () => mql.removeListener(listener)
+}
 
 
-// TODO: detext if the script runs in js or esm (if dirname is available)
 
-
-// RUNTIME
+////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////// RUNTIME ////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
 // Fully functional Node & core modules.
 // it is true for Node.js, Electron, NW.JS
 // it is false for browsers with shims or bundles of some Node modules (shimmed process, EventEmitter, etc..)
-var node = typeof process !== 'undefined'
-		&& !!process.versions
-		&& !!process.versions.node
+p.node = typeof process !== 'undefined'
+	&& !!process.versions
+	&& !!process.versions.node
 
 // Detects if the APP is launched as standalone Progressive Web App. Still a website, but looking like OS app, without url bar.
 // TODO: WARNING: this returns true even for popup windows created with window.open()
-var pwa = hasWindow
-		&& window.matchMedia('(display-mode: standalone)').matches
-		&& (document.head.querySelector('[rel="manifest"]') !== null)
+p.pwa = p.window
+	&& window.matchMedia('(display-mode: standalone)').matches
+	&& (document.head.querySelector('[rel="manifest"]') !== null)
 
 // Windows 10 app - Universal Windows Platform.
-var uwp = typeof Windows !== 'undefined'
-		&& typeof MSApp !== 'undefined'
+p.uwp = typeof Windows !== 'undefined'
+	&& typeof MSApp !== 'undefined'
 
 // Node + Chromium
-var nwjs     = !!(node && process.versions.nw)
-var electron = !!(node && process.versions.electron)
+p.nwjs     = !!(p.node && process.versions.nw)
+p.electron = !!(p.node && process.versions.electron)
 
 // Cordova mobile app
-var cordova = hasWindow && !!window.cordova
+p.cordova = !!(p.window && window.cordova)
 
 // Chrome app (Chrome OS app)
-var chromeapp = undefined // todo
+p.chromeapp = false // todo
 
 // Is the app just a plain old website, in a browsers, without node or any other special system bindings.
-var web     = !node && !uwp && !nwjs && !electron && !cordova && !chromeapp// && typeof window === 'object'
-var browser = !node && !uwp && !nwjs && !electron && !cordova && !chromeapp && !pwa// && typeof window === 'object'
+p.web     = !p.node && !p.uwp && !p.nwjs && !p.electron && !p.cordova && !p.chromeapp// && typeof window === 'object'
+p.browser = !p.node && !p.uwp && !p.nwjs && !p.electron && !p.cordova && !p.chromeapp && !p.pwa
 
 // Script is executed inside Web Worker
-var worker = !hasWindow
-			&& typeof self !== 'undefined'
-			&& !!self.importScripts
-			&& !!self.close
+p.worker = !p.window
+	&& typeof self !== 'undefined'
+	&& self.importScripts !== undefined
+	&& self.close !== undefined
+
+p.serviceWorker = undefined // TODO
 
 
 
-// OS
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////// OS //////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
-var windows  = node ? process.platform === 'win32'  : ua.includes('Windows')
-var linux    = node ? process.platform === 'linux'  : undefined // TODO: detect in browser
-var macosx   = node ? process.platform === 'darwin' : ua.includes('Macintosh')
-var ios      = undefined // TODO
-var chromeos = hasWindow && ua.includes('CrOS') // TODO
-var android  = hasWindow && ua.includes('Android') // TODO
+// TODO: figure out if these should be mutually exclusive (Tizen and Android are based on linux and have Linux in their user agents)
+p.windows  = p.node ? process.platform === 'win32'  : ua.includes('Windows')
+p.macosx   = p.node ? process.platform === 'darwin' : ua.includes('Macintosh')
+p.linux    = p.node ? process.platform === 'linux'  : ua.includes('Linux') // TODO: detect in browser properly. NOTE: Android and Tizen are based on Linux and will result in true
+p.ios      = false // TODO
+p.chromeos = p.window ? ua.includes('CrOS') : false // TODO
+p.android  = p.window ? ua.includes('Android') : false // TODO
+p.tizen    = p.window ? ua.includes('Tizen') : false // TODO
 
 
 
-// BROWSER / RENDERING ENGINE
+////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////// BROWSER ////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
 // These return true if their rendering engine is used to render the app.
 // I.e: UWP apps are rendered by Edge's EdgeHTML, Electron/NWJS apps are rendered by Chromium.
 
 // https://blogs.windows.com/msedgedev/2017/10/05/microsoft-edge-ios-android-developer/#49Fi4TfpgzHAwuXQ.97
-//var edgeAndroid = hasWindow && ua.includes('EdgiOS/')
-//var edgeIos     = hasWindow && ua.includes('EdgA/')
-var edgeWindows = hasWindow && ua.includes('Edge/')
+p.edgeAndroid = p.window && ua.includes('EdgiOS/')
+p.edgeIos     = p.window && ua.includes('EdgA/')
+p.edgeWin     = p.window && ua.includes('Edge/')
 // NOTE: Only returnin true for Edge (EdgeHtml) on windows. Android/iOs versions of Edge are powered by Webkit.
-var edge        = edgeWindows
-var chrome      = hasWindow && ua.includes('Chrome') && !ua.includes('Edge/') // TODO: verify
-var safari      = hasWindow && ua.includes('Safari') // TODO: verify
-//var firefox     = undefined // TODO
-//var webkit  = undefined // TODO
-//var v8      = undefined // TODO
-//var chakra  = undefined // TODO
+p.edge    = p.edgeWin
+p.chrome  = p.window && ua.includes('Chrome') && !p.edge
+p.safari  = p.window && ua.includes('Safari') // TODO: verify
+p.firefox = undefined // TODO
+
+// RENDERING & JS ENGINES
+
+p.edgeHtml = p.edgeWin
+p.webkit   = p.chrome || p.edgeAndroid || p.edgeIos || p.safarai
+p.blink    = p.chrome || p.edgeAndroid || p.opera
+//p.gecko    = p.firefox
 
 
-// FORM FACTOR / DEVICE FEATURES
 
-var touch = typeof navigator !== 'undefined' && navigator.maxTouchPoints > 0
-// TODO: something like hasKeyboard, hasMouse, hasInk so this lib could be helpful
-// in variety of uwp, node, or iot apps.
-// TODO: form factor like desktop, mobile, tablet, laptop
+////////////////////////////////////////////////////////////////////////////////
+//////////////////// SCREEN,  FORM FACTOR, DEVICE FEATURES ////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+registerQuery('(pointer: coarse)', bool => {
+	p.touch = bool
+	p.mouse = !bool
+	p.pointer = bool ? 'coarse' : 'fine'
+	p.tabletMode = bool
+	p.onupdate && p.onupdate()
+})
+
+registerQuery('(orientation: portrait)', bool => {
+	p.portrait = bool
+	p.landscape = !bool
+	p.orientation = bool ? 'portrait' : 'landscape'
+	p.onupdate && p.onupdate()
+})
+
+p.touchscreen = typeof navigator !== 'undefined' && navigator.maxTouchPoints > 0
+
+if (p.window) {
+		p.pixelRatio = parseFloat(window.devicePixelRatio.toFixed(2))
+	// TODO: phone, table, desktop, tv
+	if (ua.includes('TV'))
+		p.devieType = 'tv'
+	else
+		p.devieType = 'desktop'
+}
 
 
-// OTHER
+
+////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////// OTHER ////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
 // Detects if the platform is constrained by Cancerous Security Policy.
-var csp = uwp || chromeapp
+p.csp = p.uwp || p.chromeapp
 // Detects if NW.JS runs in SDK version (console available) and if Electron is executed from npm/node_modules/electron global.
 // TODO: would be nice to detect if UWP is attached to Visual Studio debugger.
-var sdk = false
-if (nwjs) {
-	sdk = process.versions['nw-flavor'] === 'sdk'
-} else if (electron) {
-	sdk = process.execPath.replace(/\\/g, '/').includes('node_modules/electron/')
-} else if (uwp) {
-	sdk = Windows.ApplicationModel.Package.current.isDevelopmentMode
-} else if (browser) {
-	if (hasWindow) {
+p.dev = false
+if (p.nwjs) {
+	p.dev = process.versions['nw-flavor'] === 'sdk'
+} else if (p.electron) {
+	p.dev = process.execPath.replace(/\\/g, '/').includes('node_modules/electron/')
+} else if (p.uwp) {
+	p.dev = Windows.ApplicationModel.Package.current.isDevelopmentMode
+} else if (p.browser) {
+	if (p.window) {
 		// Matching window size is a good first indicator but it isn't bulletproof.
 		// It won't work when devtools are detached from main window or in chrome remote debugging.
-		sdk  = window.outerWidth  - window.innerWidth  > 50
+		p.dev = window.outerWidth - window.innerWidth > 50
 			// NOTE: height has to incorporate at least 88px tall toolbar (even greater with touch UI).
 			|| window.outerHeight - window.innerHeight > 140
 	}
-	if (!sdk) {
+	if (!p.dev) {
 		// toString on object is only called in developer tools in console.
 		let temp = /./
-		temp.toString = () => sdk = true
+		temp.toString = () => p.dev = true
 		console.log('%c', temp)
 	}
+} else if (p.node) {
+	p.dev = process.env.NODE_ENV !== 'production'
 }
 // Supports service workers
 //var supportsServiceWorker = typeof navigator !== 'undefined' && !!navigator.serviceWorker && !!navigator.serviceWorker.register
 
-export default {
-	// system
-	windows, android, chromeos,
-	// rendering engine
-	edge, chrome, safari,
-	// runtime
-	node, web, browser,
-	pwa, uwp, cordova, chromeapp, nwjs, electron,
-	nw: nwjs,
-	// form factor
-	touch,
-	// app features
-	hasWindow,
-	isConsole,
-	window: hasWindow,
-	console: isConsole,
-	// other
-	csp,
-	sdk,
-	dev: sdk,
-}
+
 
 /*
 
